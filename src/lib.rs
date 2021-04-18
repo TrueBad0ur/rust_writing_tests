90552fbbcb66c78741f0c2061c8b5e42cca31d77
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2021-04-19 01:01:22 +0300

    assert_eq test

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,7 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
+#[cfg(test)]
 
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
+pub fn add_two(a: i32) -> i32 {
+    a + 2
 }
 
 #[cfg(test)]
@@ -15,30 +9,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(larger.can_hold(&smaller));
-    }
-
-    #[test]
-    fn smaller_can_hold_larger() {
-        let larger = Rectangle {
-            width: 10,
-            height: 10,
-        };
-        let smaller = Rectangle {
-            width: 2,
-            height: 2,
-        };
-
-        assert!(smaller.can_hold(&larger));
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
     }
 }
