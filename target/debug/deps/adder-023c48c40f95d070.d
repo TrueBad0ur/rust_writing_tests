53ca4550aab7e93ec88318ef21949774801a04b8
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2021-04-19 00:24:37 +0300

    Start tests

---
+++ target/debug/deps/adder-023c48c40f95d070.d
@@ -0,0 +1,5 @@
+C:\Users\truebad0ur\Documents\rust_tests\adder\target\debug\deps\adder-023c48c40f95d070.exe: src\lib.rs
+
+C:\Users\truebad0ur\Documents\rust_tests\adder\target\debug\deps\adder-023c48c40f95d070.d: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/adder-1453d9cba7d4e1e4.d
@@ -0,0 +1,7 @@
+C:\Users\truebad0ur\Documents\rust_tests\adder\target\debug\deps\adder-1453d9cba7d4e1e4.rmeta: src\lib.rs
+
+C:\Users\truebad0ur\Documents\rust_tests\adder\target\debug\deps\libadder-1453d9cba7d4e1e4.rlib: src\lib.rs
+
+C:\Users\truebad0ur\Documents\rust_tests\adder\target\debug\deps\adder-1453d9cba7d4e1e4.d: src\lib.rs
+
+src\lib.rs:
