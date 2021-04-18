cfb2a443c9113edfdf107e69d8ac637ce5462cc8
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2021-04-19 01:14:47 +0300

    should_panic macro with custom expected

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
-#[cfg(test)]
+pub struct Guess {
+    value: i32,
+}
 
-pub fn greeting(name: &str) -> String {
-    format!("Hello {}!", name)
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        }
+
+        Guess { value }
+    }
 }
 
 #[cfg(test)]
@@ -9,12 +25,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Carol");
-        assert!(
-            result.contains("jCarol"),
-            "Greeting did not contain name, value was `{}`",
-            result
-        );
+    #[should_panic(expected = "Guess value must be less than or equal to 100, got ")]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
